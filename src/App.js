e2722fd62762a263fcf6e66eb050f16ce045892a
OscarDikma <oscardikma@gmail.com>
2023-07-17 16:42:46 -0300

    alterando as rotas para o aplicativo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
-
 function App() {
   return (
     <Router>
@@ -14,4 +13,5 @@ function App() {
 }
 
 export default App;
+
 /**teste */
